30fa083bcbe0671eeaade4752c769e6ab8e7eda9
likun <630662717@qq.com>
2023-10-01 23:56:28 +0800

    aaaaaa

--- example/views.py
+++ example/views.py
@@ -18,4 +18,4 @@ def api(request):
     r = requests.request(method='GET', url='https://api.twitter.com/1.1/users/show.json?screen_name=' + name,
                          headers={
                              'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAG7xqAEAAAAAhX%2FIUoT9ShGkFjPZWohUodOv6F4%3DiTI6bjcnwCsnQnHnPEwGFS2xQWkGeu6Q9oqsBrMOvZnGhE9jrt'})
-    return HttpResponse(r)
+    return HttpResponse('123')
